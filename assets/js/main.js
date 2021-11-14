34f3436adf648ce97daf243ba06bc4848cbbc802
Michael Zephyr <mzephyr@nvidia.com>
2021-11-14 00:19:23 -0800

    Initial Push

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+(function() {
+  
+  "use strict";  
+
+      //WOW Scroll Spy
+      var wow = new WOW({
+        //disabled for mobile
+        mobile: false
+      });
+      wow.init();
+
+})();
