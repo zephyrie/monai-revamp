a6f5eccd3062058da695b81ee49ca3a331982dfd
Michael Zephyr <mzephyr@nvidia.com>
2021-11-16 20:30:10 -0800

    Index page done, need responsive design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,73 @@
 (function() {
-  
-  "use strict";  
 
-      //WOW Scroll Spy
-      var wow = new WOW({
+    "use strict";
+
+    //WOW Scroll Spy
+    var wow = new WOW({
         //disabled for mobile
         mobile: false
-      });
-      wow.init();
+    });
+    wow.init();
+
+    // Main.js
+    /* ---- particles.js config ---- */
+
+    particlesJS("hero-area", {
+        "particles": {
+            "number": {
+                "value": 80,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#5dc1b7"
+            },
+            "opacity": {
+                "value": 0.2,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 4,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 200,
+                "color": "#5d5d5d",
+                "opacity": .1,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "retina_detect": true
+    });
+
 
 })();
